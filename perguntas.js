7e0c13271e6260aadc9225c545c666857f0d76fb
zigfriddy09 <o.guilherme25@escola.pr.gov.br>
2025-08-11 08:55:31 -0300

    ㅤ

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,35 @@
 criaCartao(
-    categoria,
-    pergunta,
-    resposta
+    'Carro',
+    ',Qual o carro mais forte do mundo ?'
+    'Uno com escada'
 )
 
 criaCartao(
-    categoria,
-    pergunta,
-    resposta
+    'Automóveis',
+    'Carroça paga IPVA ?,
+    'Não'
 )
 
 criaCartao(
-    categoria,
-    pergunta,
-    resposta
+    'Caminhões',
+    'Qual caminhão mais forte do mundo',
+    'Mercedes Benz 1113'
+)
+
+criaCartao(
+    'Ônibus',
+    'O que é um ônibus DD',
+    'Um ônibus de 2 andares'
+)
+
+criaCartao(
+    'Carros',
+    'Que marca é o corsel ?',
+    'Ford'
+)
+
+criaCartao(
+    'Mecânica',
+    'O que é um carburador ?',
+    'Um carburador é um dispositivo mecânico usado em motores de combustão interna para misturar ar e combustível em uma proporção correta, criando a mistura inflamável que será queimada para gerar energia. Ele funciona criando um vácuo que suga o combustível e o mistura com o ar, transformando-o numa névoa fina que é então enviada para os cilindros do motor. Embora amplamente substituídos por sistemas de injeção eletrônica em carros modernos, os carburadores ainda são encontrados em motores pequenos, como os de cortadores de grama, geradores e algumas motocicletas.'
 )
